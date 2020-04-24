a142046ae5eae20804d710cabd49e7dce297ff64
Alfonso Roman Zubeldia <alfonsoromanz24@gmail.com>
2020-04-24 14:41:52 -0300

    Lambda proxy: construct real URI with path params

--- lambdas/lambda-proxy/index.js
+++ lambdas/lambda-proxy/index.js
@@ -2,9 +2,33 @@ const fetch = require('node-fetch');
 const aws = require('aws-sdk');
 const lambda = new aws.Lambda({ region: 'us-east-1'});
 
+
+const replaceAll = function (originalStr, searchStr , replaceStr) {
+  const str = originalStr;
+  searchStr = searchStr.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+  return str.replace(new RegExp(searchStr, 'gi'), replaceStr);
+};
+
+// Get Call Uri Function: 
+// Returns the real URI that we are going to call.
+// For instance it converts event.uri:
+// /users/{id} into /users/1
+
+const getCallUri = function(pathParams, uri) {
+  let callUri = uri;
+  Object.keys(pathParams).forEach(function (pathParam, index) {
+    const replaceValue = `{${pathParam}}`;
+    const value = pathParams[pathParam];
+    callUri = replaceAll(callUri, replaceValue, pathParams[pathParam]);
+  });
+  return callUri;
+}
+
 exports.handler = async (event,context,callback) => {
     let payload = JSON.stringify(event);
     let response = null;
+
+    //return event
     // Before hooks
     try {
         for (var _i = 0; _i < event.hooksBefore.length; _i++) {
@@ -14,10 +38,12 @@ exports.handler = async (event,context,callback) => {
     } catch (err) {
         callback(err,null);
     }
-
+    
     //call realAPI with response
-    const apiResponse = await fetch(event.uri,{ method: event.method, body: payload, headers: event.headers });
+    let callUri = getCallUri(event.pathParams, event.uri);
+    const apiResponse = await fetch(callUri,{ method: event.method, body: payload, headers: event.headers });
     const json = await apiResponse.json();
+    
     // after
     payload = json;
     response = payload;
